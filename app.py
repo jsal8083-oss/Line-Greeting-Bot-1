1412376e2332999b34e92c5e36209ee0bf18c368
jsal8083-oss <jsal8083@gmail.com>
2026-06-14 15:30:22 -0500

    Added welcome message

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import os
-from flask import Flask, request
+
+from flask import Flask, request, abort
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    TextMessage,
+    ReplyMessageRequest
+)
+
+from linebot.v3.webhooks import MemberJoinedEvent
 
 app = Flask(__name__)
+
+CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
+CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
+
+configuration = Configuration(
+    access_token=CHANNEL_ACCESS_TOKEN
+)
+
+handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/")
 def home():
@@ -10,19 +31,38 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
 
-    body = request.json
+    signature = request.headers.get("X-Line-Signature")
 
-    print("Webhook received:")
-    print(body)
+    body = request.get_data(as_text=True)
 
-    events = body.get("events", [])
+    try:
+        handler.handle(body, signature)
+    except Exception as e:
+        print(e)
+        abort(400)
 
-    for event in events:
+    return "OK"
 
-        if event.get("type") == "memberJoined":
-            print("A new member joined!")
+@handler.add(MemberJoinedEvent)
+def handle_member_join(event):
 
-    return "OK", 200
+    with ApiClient(configuration) as api_client:
+
+        line_bot_api = MessagingApi(api_client)
+
+        welcome_text = (
+            "🎉 Welcome to the INFOCHAT bulletin for MCOC related information and infographs!\n\n"
+            "This chat is READ ONLY! DO NOT post in this chat unless you have been given permission!."
+        )
+
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    TextMessage(text=welcome_text)
+                ]
+            )
+        )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
