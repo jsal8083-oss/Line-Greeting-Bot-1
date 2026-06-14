b427c74ff32f8ccb7f1ecc82991dc8c2b43ae4bc
jsal8083-oss <jsal8083@gmail.com>
2026-06-14 14:53:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "LINE Bot Running"
+    return "LINE Greeting Bot Running"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+
     body = request.json
 
+    print("Webhook received:")
     print(body)
 
-    return "OK"
+    events = body.get("events", [])
+
+    for event in events:
+
+        if event.get("type") == "memberJoined":
+            print("A new member joined!")
+
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
